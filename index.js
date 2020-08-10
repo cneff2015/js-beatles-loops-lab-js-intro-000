c7cadf4d49e206ff3eb9160b9c4cf14c5910bf58
Cale Neff <cale.neff@yahoo.com>
2020-08-10 12:48:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // add solution here
+//for loop
 function theBeatlesPlay (musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
+//while loop
+function johnLennonFacts(array) {
+  var i = 0;
+  while (i < array.length) {
+    array[i] = array[i] + "!!!";
+    i++;
   }
   return array;
 }
