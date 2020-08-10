5d321cd078394119424e37a1e23c032f19f2b845
Cale Neff <cale.neff@yahoo.com>
2020-08-10 12:43:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[1]} plays ${instruments[1]}`)
+  }
+  return array;
+}
